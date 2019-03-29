de36c3901ffbb5b1ad98512b5514b519b4022db0
Sylvain Boily <sylvain@wazo.io>
2019-03-28 22:06:26 -0400

    Fix chat data

--- wazo_mobile/webhookd/plugin.py
+++ wazo_mobile/webhookd/plugin.py
@@ -136,8 +136,8 @@ class PushNotification(object):
             channel_id = 'wazo-notification-voicemail'
 
         if is_message:
-            message_title = data['data']['alias']
-            message_body = data['data']['content']
+            message_title = data['items']['alias']
+            message_body = data['items']['content']
             channel_id = 'wazo-notification-chat'
 
         if self.apns_token and channel_id == 'wazo-notification-call':
