629a9be494ec988c7806608bb9b1a05a1ae2b95e
Mehdi Abaakouk <mabaakouk@wazo.io>
2019-06-05 17:03:27 +0200

    Use new webhook plugin format

--- wazo_mobile/webhookd/plugin.py
+++ wazo_mobile/webhookd/plugin.py
@@ -19,10 +19,8 @@ class Service:
 
     def load(self, dependencies):
         bus_consumer = dependencies['bus_consumer']
-        celery_app = dependencies['celery']
         self.config = dependencies['config']['mobile']
         self.subscription_service = SubscriptionService(dependencies['config'])
-        self.token = self.get_token()
         bus_consumer.subscribe_to_event_names(uuid=uuid.uuid4(),
                                               event_names=['auth_user_external_auth_added'],
                                               # tenant_uuid=None,
@@ -36,40 +34,6 @@ class Service:
                                               wazo_uuid=None,
                                               callback=self.on_external_auth_deleted)
         print('Mobile push notification plugin is started')
-
-        @celery_app.task
-        def mobile_push_notification(subscription, event):
-            user_uuid = subscription.get('events_user_uuid')
-            # TODO(sileht): We should also filter on tenant_uuid
-            # tenant_uuid = subscription.get('events_tenant_uuid')
-            if (event['data']['user_uuid'] == user_uuid
-                    # and event['data']['tenant_uuid'] == tenant_uuid
-                    and event['name'] in ['chatd_user_room_message_created',
-                                          'call_created']):
-                return
-
-            data, external_config = self.get_external_token(user_uuid)
-            token = data.get('token')
-            apns_token = data.get('apns_token')
-            push = PushNotification(token, self.config, apns_token, external_config)
-
-            msg = None
-            data = event.get('data')
-            name = event.get('name')
-
-            if name == 'user_voicemail_message_created':
-                msg = dict(notification_type='voicemailReceived', items=data)
-
-            if name == 'call_push_notification':
-                msg = dict(notification_type='incomingCall', items=data)
-
-            if name == 'chatd_user_room_message_created':
-                msg = dict(notification_type='messageReceived', items=data)
-
-            if msg:
-                push.send_notification(msg)
-
-        self._callback = mobile_push_notification
 
     def on_external_auth_added(self, body, event):
         if body['data'].get('external_auth_name') == 'mobile':
@@ -105,26 +69,22 @@ class Service:
             for subscription in subscriptions:
                 self.subscription_service.delete(subscription.uuid, [tenant_uuid])
 
-    def get_token(self):
+    @classmethod
+    def get_token(cls, config):
         auth = Auth(
-            self.config['auth']['host'],
-            username=self.config['auth']['username'],
-            password=self.config['auth']['password'],
+            config['auth']['host'],
+            username=config['auth']['username'],
+            password=config['auth']['password'],
             verify_certificate=False)
         return auth.token.new('wazo_user', expiration=3600)
 
-    def get_external_token(self, user_uuid):
-        token = None
-        auth = Auth(self.config['auth']['host'], verify_certificate=False, token=self.token['token'])
-        try:
-            token = auth.external.get('mobile', user_uuid)
-        except:
-            self.token = self.get_token()
-            auth = Auth(self.config['auth']['host'], verify_certificate=False, token=self.token['token'])
-            token = auth.external.get('mobile', user_uuid)
-
+    @classmethod
+    def get_external_token(cls, config, user_uuid):
+        auth = Auth(config['auth']['host'], verify_certificate=False,
+                    token=cls.get_token(config))
+        token = auth.external.get('mobile', user_uuid)
         tenant_uuid = auth.users.get(user_uuid).get('tenant_uuid')
-        external_config = self._get_external_config(auth, tenant_uuid)
+        external_config = auth.external.get_config('mobile', tenant_uuid)
         external_config['ios_apns_cert'] = '/tmp/ios.pem'
 
         with open(external_config['ios_apns_cert'], 'w') as cert:
@@ -133,11 +93,39 @@ class Service:
 
         return (token, external_config)
 
-    def callback(self):
-        return self._callback
+    @classmethod
+    def run(cls, task, config, subscription, event):
+        config = config['mobile']
 
-    def _get_external_config(self, auth, tenant_uuid):
-        return auth.external.get_config('mobile', tenant_uuid)
+        user_uuid = subscription.get('events_user_uuid')
+        # TODO(sileht): We should also filter on tenant_uuid
+        # tenant_uuid = subscription.get('events_tenant_uuid')
+        if (event['data']['user_uuid'] == user_uuid
+                # and event['data']['tenant_uuid'] == tenant_uuid
+                and event['name'] in ['chatd_user_room_message_created',
+                                      'call_created']):
+            return
+
+        data, external_config = cls.get_external_token(config, user_uuid)
+        token = data.get('token')
+        apns_token = data.get('apns_token')
+        push = PushNotification(token, config, apns_token, external_config)
+
+        msg = None
+        data = event.get('data')
+        name = event.get('name')
+
+        if name == 'user_voicemail_message_created':
+            msg = dict(notification_type='voicemailReceived', items=data)
+
+        if name == 'call_push_notification':
+            msg = dict(notification_type='incomingCall', items=data)
+
+        if name == 'chatd_user_room_message_created':
+            msg = dict(notification_type='messageReceived', items=data)
+
+        if msg:
+            push.send_notification(msg)
 
 
 class PushNotification(object):
